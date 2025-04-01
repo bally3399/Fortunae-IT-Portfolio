ed093b1188f6cd48352f5c6d9127c349fa22414c
Baliqis <sulaimabaliqis@gmail.com>
2025-04-01 21:40:15 +0100

    added new mail

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
                     Great Together.
                 </p>
                 <p>Send us an email</p>
-                <a href="mailto:contact@toldbyfortunae.com">contact@toldbyfortunae.com</a>
+                <a href="mailto:contact@fortunaeitsolutions.com">contact@toldbyfortunae.com</a>
                 <p>Find us</p>
                 <h4>Manchester, United Kingdom</h4>
                 <br />
